509e05f6fd36ac38d9ba519b9c49b2e8ee1bb2cc
Aditya Jayant Gupta <daemonad@protonmail.com>
2022-07-04 20:21:58 +0530

    testing database

---
+++ data/images.js
@@ -0,0 +1,27 @@
+export const images = [
+  {
+    id: 1,
+    path: "/static/1.jpg",
+    type: "photo",
+  },
+  {
+    id: 2,
+    path: "/static/2.jpg",
+    type: "photo",
+  },
+  {
+    id: 3,
+    path: "/static/3.jpg",
+    type: "psd",
+  },
+  {
+    id: 4,
+    path: "/static/4.jpg",
+    type: "vector",
+  },
+  {
+    id: 5,
+    path: "/static/5.jpg",
+    type: "vector",
+  },
+];
