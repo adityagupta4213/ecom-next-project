fda5215d57bb9208c0c63b3dd24a9458400bb700
Aditya Jayant Gupta <daemonad@protonmail.com>
2022-07-08 17:41:38 +0530

    wrap app in SSRProvider for Bootstrap compatibility

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,14 +1,17 @@
+import SSRProvider from "react-bootstrap/SSRProvider";
 import NavBar from "../components/NavBar";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "/styles/globals.scss";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
 export default function App({ Component, pageProps }) {
+  // App wrapped in SSRProvider for Bootstrap ID consistency
   return (
-    <div>
+    <SSRProvider>
       <NavBar />
       <Component {...pageProps} />
-    </div>
+    </SSRProvider>
   );
 }
