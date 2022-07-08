380e5fe040be56e975c3554097d3b24ce5b604d4
Aditya Jayant Gupta <daemonad@protonmail.com>
2022-07-08 17:37:05 +0530

    Add tags to images

--- data/images.js
+++ data/images.js
@@ -3,25 +3,30 @@ export const images = [
     id: 1,
     path: "/static/1.jpg",
     type: "photo",
+    tags: ["nature", "mountains"],
   },
   {
     id: 2,
     path: "/static/2.jpg",
     type: "photo",
+    tags: ["urban", "buildings"],
   },
   {
     id: 3,
     path: "/static/3.jpg",
     type: "psd",
+    tags: ["nature", "flowers"],
   },
   {
     id: 4,
     path: "/static/4.jpg",
     type: "vector",
+    tags: ["nature", "river"],
   },
   {
     id: 5,
     path: "/static/5.jpg",
     type: "vector",
+    tags: ["abstract", "art"],
   },
 ];
