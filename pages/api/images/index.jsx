c377862d466ef91b5fd2db4a436da6fa51164e90
Aditya Jayant Gupta <daemonad@protonmail.com>
2022-07-08 17:41:08 +0530

    fix image api for better usability

--- pages/api/images/index.jsx
+++ pages/api/images/index.jsx
@@ -1,12 +1,29 @@
 import { images } from "../../../data/images";
 
+/**
+ * Handles fetching images based on image "type" or "id"
+ * @param {Object} req Incoming request object
+ * @param {Object} res Outgoing response object
+ * @returns {Array} "responseImages" as the array of matching image objects
+ */
 export default function handler(req, res) {
-  // if no "type" is specified, return all images; else filter
-  var responseImages =
-    req.query.type == "undefined"
-      ? images
-      : images.filter((image) => {
-          return image.type == req.query.type;
-        });
+  // Handle the request based on params
+  var responseImages = [];
+  // If a "type" param is specified
+  if (req.query.type) {
+    responseImages = images.filter((image) => {
+      return image.type == req.query.type;
+    });
+  }
+  // If an "id" param is specified
+  if (req.query.id) {
+    responseImages = images.filter((image) => {
+      return image.id == req.query.id;
+    });
+  }
+  // If nothing is specified
+  else {
+    responseImages = images;
+  }
   res.status(200).json(responseImages);
 }
--- pages/catalog/index.jsx
+++ pages/catalog/index.jsx
@@ -6,22 +6,19 @@ export default function Catalog() {
   // state for keeping track of fetched images
   const [images, setImages] = useState([]);
   const router = useRouter();
+  // parameter string for the API, include the image "type" if specified in the route
+  const paramString = "/api/images?" + router.query.type || undefined;
 
   /**
    *  Fetches images from the API and updates the state
    */
   const fetchImages = async () => {
-    const response = await fetch(
-      "/api/images?" +
-        new URLSearchParams({
-          type: router.query.type,
-        })
-    );
+    const response = await fetch(paramString);
     const data = await response.json();
     // update state
     setImages(data);
   };
-  
+
   // fetch images on every page route change
   useEffect(() => {
     fetchImages();
